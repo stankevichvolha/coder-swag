78f04e3a14c78306393f3660228d92dd3871f3f8
Volha Stankevich <stankevichvolhaig@gmail.com>
2019-06-21 10:30:00 -0700

    start

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Volha  Stankevich on 6/21/19.
+//  Copyright © 2019 Volha  Stankevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
